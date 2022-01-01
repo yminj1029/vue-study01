b3ac3422c30dcca433ea6fa3fc25194b70e64089
MinjiYun <minjiyun204@gmail.com>
2022-01-01 21:10:20 +0900

    session10~11 vue-cli study

---
+++ vue-cli/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+      <app-header 
+        v-bind:propsdata="str"
+        v-on:renew="renewStr"
+      ></app-header>
+  </div>
+</template>
+
+<script>
+import AppHeader from './components/AppHeader.vue';
+
+export default {
+    //data 중복 피하기 위해 function 사용
+    data:function(){
+        return{
+            str:'Header'
+        }
+    },
+    components:{
+        'app-header':AppHeader
+    },
+    methods:{
+        renewStr:function(){
+            this.str = 'hi';
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue-cli/src/a.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>header</div>
+</template>
+
+<script>
+export default {
+ //자바스크립트 인스턴스 옵션
+}
+</script>
+
+<style>
+/* css */
+</style>
